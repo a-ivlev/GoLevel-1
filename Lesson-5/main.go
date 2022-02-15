c6d27c3c7d0f12e16320dab68025a7df7bf10725
Aleksandr <a.y.ivlev@gmail.com>
2022-02-15 11:03:35 +0300

    Add cache in func fibonacci clssik

--- Lesson-5/main.go
+++ Lesson-5/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-
+// fibonacci рекурсивный метод вычисления числа Фибоначчи.
 func fibonacci(n uint64) uint64 {
 	if n == 2 || n == 1 {
 		return 1
@@ -11,7 +11,33 @@ func fibonacci(n uint64) uint64 {
 	return fibonacci(n-1) + fibonacci(n-2)
 }
 
-// Оптимизация приложения за счёт сохранения предыдущих результатов в мапе.
+// fibonacciClassic классический метод вычисления числа Фибоначчи.
+func fibonacciClassic(n uint64) uint64 {
+	var f1, f2 uint64
+	f2 = 1
+
+	for i := uint64(0); i < n-1; i++ {
+		f1, f2 = f2, f1+f2
+	}
+
+	return f2
+}
+
+func fibonacciClassicCache(n uint64, cache map[uint64]uint64) uint64 {
+	var f1, f2 uint64
+	f2 = 1
+
+	if _, ok := cache[n]; !ok {
+		for i := uint64(0); i < n-1; i++ {
+			f1, f2 = f2, f1+f2
+			cache[i+2] = f2
+		}
+	}
+
+	return cache[n]
+}
+
+// fibonacciCache Оптимизация приложения за счёт сохранения предыдущих результатов в мапе.
 func fibonacciCache(n uint64, cache map[uint64]uint64) uint64 {
 	if n == 2 || n == 1 {
 		return 1
